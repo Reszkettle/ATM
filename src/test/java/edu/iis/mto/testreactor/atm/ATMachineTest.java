168a3e37bf04ce821142d9329fa3919852c50858
Jakub Reszka <jakub.reszka@accenture.com>
2021-04-24 17:11:41 +0200

    TEST: added test for case when requested withdraw amount has invalid currency

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,18 +2,60 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Currency;
+
+@ExtendWith(MockitoExtension.class)
 class ATMachineTest {
 
+    private static final Currency DEFAULT_CURRENCY = Money.DEFAULT_CURRENCY;
+    private static final String DEFAULT_CARD_NUMBER = "5123456789104444";
+    private static final PinCode DEFAULT_PIN_CODE = PinCode.createPIN(1, 2, 3, 4);
+    private static final Card DEFAULT_CARD = Card.create(DEFAULT_CARD_NUMBER);
+
+    @Mock
+    private Bank bank;
+
+    @Mock
+    private MoneyDeposit moneyDeposit;
+
+    private ATMachine atm;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() {
+        atm = new ATMachine(bank, DEFAULT_CURRENCY);
+    }
 
     @Test
     void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
+    }
+
+    @Test
+    void shouldThrowExceptionWithWrongCurrencyErrorCodeWhenAmountCurrencyIsInvalid() {
+        // given
+        Money invalidAmount = new Money(20, Currency.getInstance("EUR"));
+        ErrorCode expectedErrorCode = ErrorCode.WRONG_CURRENCY;
+
+        // when
+        ErrorCode actualErrorCode = null;
+        try {
+            atm.withdraw(DEFAULT_PIN_CODE, DEFAULT_CARD, invalidAmount);
+            Assertions.fail("Should throw ATMOperationException when amount is invalid");
+        } catch (ATMOperationException e) {
+            actualErrorCode = e.getErrorCode();
+        }
+
+        // then
+        Assertions.assertEquals(expectedErrorCode, actualErrorCode);
     }
 
 }
